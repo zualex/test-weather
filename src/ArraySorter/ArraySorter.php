0c28602424b32e7068032bb62d1b02fc8c7d9d82
zualex <Azubarev32@yandex.ru>
2019-12-16 22:47:14 +0300

    Add strict type 1

--- src/ArraySorter/ArraySorter.php
+++ src/ArraySorter/ArraySorter.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Weather\ArraySorter;
 
 class ArraySorter
--- src/Storage/Exception/InvalidDriver.php
+++ src/Storage/Exception/InvalidDriver.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Weather\Storage\Exception;
 
 class InvalidDriver extends \Exception
